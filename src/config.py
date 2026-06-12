02bc75211614e5560ced836e251915ee041c6e2b
Maxim Prestwich <maximprestwich1@gmail.com>
2026-06-12 15:31:46 +0000

    implement training and evaluation functions and training loop for baseline cnn+lstm

--- src/config.py
+++ src/config.py
@@ -4,3 +4,4 @@ from pathlib import Path
 # paths
 PROJECT_ROOT = Path("/homes/mp2940/violence-detection-dissertation")
 DATASET_ROOT = Path("/homes/mp2940/demo/datasets/rwf-2000/RWF-2000")
+CHECKPOINT_DIR = Path("/homes/mp2940/violence-detection-dissertation/checkpoints")
