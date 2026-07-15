a94a02fb939aa0a12e80aa8c96937a9dd1b0c7fc
Maxim Prestwich <maximprestwich1@gmail.com>
2026-07-15 12:39:58 +0000

    implement rise class

---
+++ src/XAI/rise.py
@@ -0,0 +1,156 @@
+import torch
+import torch.nn.functional as F
+
+class RISE:
+    def __init__(self, model, num_masks=1000, mask_batch_size=8, temporal_grid_size=8, spatial_grid_size=7):
+        self.model = model
+        self.num_masks = num_masks
+        self.mask_batch_size = mask_batch_size
+        self.temporal_grid_size = temporal_grid_size
+        self.spatial_grid_size = spatial_grid_size
+
+    def _generate_masks(self, number_of_masks, num_frames, height, width, device):
+        # 1. generate coarse masks 8x7x7
+        # 2. upscale to larger than input image 36x256x256
+        # 3. crop to original size 32x224x224
+
+        temporal_cell_size = num_frames // self.temporal_grid_size # 32 // 8 = 4
+        height_cell_size = height // self.spatial_grid_size # 224 // 7 = 32
+        width_cell_size = width // self.spatial_grid_size
+
+        # shape [num_masks, 8, 7, 7]
+        coarse_masks = (
+            torch.rand(
+                number_of_masks, 
+                self.temporal_grid_size,
+                self.spatial_grid_size, 
+                self.spatial_grid_size,
+                device=device
+            ) < 0.5
+        ).float()
+
+        # shape [num_masks, 1, 8, 7, 7]
+        coarse_masks = coarse_masks.unsqueeze(1)
+
+        enlarged_temporal_size = (1 + self.temporal_grid_size) * temporal_cell_size # (1 + 8) * 4 = 36
+        enlarged_height = (1 + self.spatial_grid_size) * height_cell_size # (1 + 7) * 32 = 256
+        enlarged_width = (1 + self.spatial_grid_size) * width_cell_size
+
+        # shape [num_masks, 1, 36, 256, 256]
+        enlarged_masks = F.interpolate(
+            coarse_masks, 
+            size=(
+                enlarged_temporal_size, 
+                enlarged_height, 
+                enlarged_width
+            ),
+            mode="trilinear",
+            align_corners=False,
+        ) 
+
+        # shape [num_masks, 36, 256, 256]
+        enlarged_masks = enlarged_masks.squeeze(1)
+
+        masks = torch.empty(
+            number_of_masks, 
+            num_frames,
+            height, 
+            width, 
+            device=device
+        )
+
+        # now cropping to shape [32, 224, 244]
+        for i in range(number_of_masks):
+            temporal_offset = torch.randint(low=0, high=temporal_cell_size, size=(1,), device=device)
+            height_offset = torch.randint(low=0, high=height_cell_size, size=(1,), device=device)
+            width_offset = torch.randint(low=0, high=width_cell_size, size=(1,), device=device)
+
+            masks[i] = enlarged_masks[
+                i,
+                temporal_offset: num_frames + temporal_offset,
+                height_offset: height + height_offset, 
+                width_offset: width + width_offset
+            ]
+        
+        return masks
+
+    def _normalise_saliency_map(self, saliency):
+        minimum = saliency.min()
+        maximum = saliency.max()
+
+        return (saliency-minimum)/(maximum-minimum + 1e-8)
+    
+    def generate_heatmap(self, input_tensor, target_class=None):
+
+        B, T, C, H, W = input_tensor.shape # 1, 32, 3, 224, 224
+        self.model.eval()
+
+        with torch.inference_mode():
+            logits = self.model(input_tensor)
+        
+        if target_class == None:
+            target_class = logits.argmax(dim=1).item()
+        else:
+            raise ValueError("target class needs to be none")
+
+        saliency_accumulator = torch.zeros(T, H, W, device=input_tensor.device)
+        masks_processed = 0 
+
+        while masks_processed < self.num_masks:
+            current_batch_size = min(self.mask_batch_size, self.num_masks - masks_processed)
+
+            # shape [current_batch_size, 32, 224, 224]
+            masks = self._generate_masks(
+                number_of_masks=current_batch_size,
+                num_frames=T, 
+                height=H, 
+                width=W,
+                device=input_tensor.device
+            )
+
+            # [1,32,3,224,224] * [batch_size,32,1,224,224] = [batch_size,32,3,224,224]
+            masked_videos = input_tensor * masks.unsqueeze(2) 
+
+            with torch.inference_mode():
+                masked_logits = self.model(masked_videos)
+                probabilities = torch.softmax(masked_logits, dim=1)
+                target_scores = probabilities[:, target_class]
+
+            weighted_masks = target_scores[:, None, None, None] * masks
+            saliency_accumulator += weighted_masks.sum(dim=0)
+            masks_processed += current_batch_size
+            print(f"processed {masks_processed}/{self.num_masks} masks")
+        
+        saliency_map = saliency_accumulator/(0.5 * self.num_masks)
+        normalised_saliency_map = self._normalise_saliency_map(saliency_map)
+
+        return (
+            saliency_map.detach(),
+            normalised_saliency_map.detach(),
+            logits.detach(),
+            target_class
+        )
+        
+                
+            
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
